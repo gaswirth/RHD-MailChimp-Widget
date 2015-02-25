7d24b05c50a07d5cbe95b42246eef30671768462
Nick Gaswirth <gaswirth@gmail.com>
2015-02-25 13:50:18 -0500

    1.0.1: Support for multiple instances, code cleanup

--- rhd-mailchimp.php
+++ rhd-mailchimp.php
@@ -29,14 +29,14 @@ class RHD_Mailchimp_Widget extends WP_Widget {
 		wp_enqueue_script( 'rhd-mailchimp-js', RHD_MC_DIR . '/rhd-mailchimp.js', array( 'jquery' ) );
 		wp_enqueue_style( 'rhd-mailchimp-css', RHD_MC_DIR . '/rhd-mailchimp.css' );
 
-		$title = apply_filters( 'widget_title', $instance['title'] );
-		$text = apply_filters( 'widget_text', empty( $instance['text'] ) ? '' : $instance['text'], $instance );
-		$button = $instance['button'] || "Submit";
+		$args['title'] = apply_filters( 'widget_title', $instance['title'] );
+		$args['text'] = apply_filters( 'widget_text', empty( $instance['text'] ) ? '' : $instance['text'], $instance );
+		$args['button'] = $instance['button'] || "Submit";
 
 		echo $before_widget;
 
-		if ( $title )
-			echo $before_title . $title . $after_title;
+		if ( $args['title'] )
+			echo $before_title . $args['title'] . $after_title;
 
 		$widget_id = substr( $this->id, -1, 1 );
 		?>
@@ -48,7 +48,7 @@ class RHD_Mailchimp_Widget extends WP_Widget {
 				<input id="rhd_mc_lname-<?php echo $widget_id; ?>" class="rhd_mc_lname" type="text" name="lname" placeholder="last">
 				<input id="rhd_mc_email-<?php echo $widget_id; ?>" class="rhd_mc_email" type="email" name="email" placeholder="subscribe">
 				<input class="rhd_mc_form_id" type="hidden" value="<?php echo $widget_id; ?>">
-				<input id="rhd_mc_submit-<?php echo $widget_id; ?>" class="rhd_mc_submit" type="submit" value="<?php echo $button; ?>" name="submit-<?php echo $widget_id; ?>">
+				<input id="rhd_mc_submit-<?php echo $widget_id; ?>" class="rhd_mc_submit" type="submit" value="<?php echo $args['button']; ?>" name="submit-<?php echo $widget_id; ?>">
 			</form>
 			<div id="rhd_mc_thanks-<?php echo $widget_id; ?>" class="rhd_mc_thanks">
 				<p>Subscribed!</p>
@@ -64,24 +64,24 @@ class RHD_Mailchimp_Widget extends WP_Widget {
 
 	public function form( $instance ) {
 		// outputs the options form on admin
-		$title = esc_attr( $instance['title'] );
-		$text = esc_textarea( $instance['text'] );
-		$button = esc_attr( $instance['button'] );
+		$args['title'] = esc_attr( $instance['title'] );
+		$args['text'] = esc_textarea( $instance['text'] );
+		$args['button'] = esc_attr( $instance['button'] );
 		?>
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Widget Title <em>(optional)</em>: </label>
-			<input id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" >
+			<input id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $args['title']; ?>" >
 		</p>
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'text' ); ?>">Text to display <em>(optional)</em>:</label>
-			<textarea class="widefat" rows="5" cols="20" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>"><?php echo $text; ?></textarea>
+			<textarea class="widefat" rows="5" cols="20" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>"><?php echo $args['text']; ?></textarea>
 		</p>
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'button'); ?>">Button text <em>(Default: Submit)</em>:</label>
-			<input id="<?php echo $this->get_field_id('button'); ?>" name="<?php echo $this->get_field_name( 'button' ); ?>" type="text" value="<?php echo $button; ?>" >
+			<input id="<?php echo $this->get_field_id('button'); ?>" name="<?php echo $this->get_field_name( 'button' ); ?>" type="text" value="<?php echo $args['button']; ?>" >
 		</p>
 
 	<?php
